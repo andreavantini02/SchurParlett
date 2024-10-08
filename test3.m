5265fd0f9c65749f0c5131997a33ff586af96b01
andreavantini02 <andreavantini02@gmail.com>
2024-10-08 15:10:14 +0200

    Update test3.m

--- test3.m
+++ test3.m
@@ -7,7 +7,7 @@ n_err = 0;
 for i = 1:n_test
     A = randn(n);
     F = fun_m(A,fun);
-    F1 = funm(A,fun);
+    F1 = funm(A,@exp);
     errRel(i) = norm(F-F1,inf)/norm(F1,inf);
     if errRel(i) > 1e-14
         n_err = n_err + 1;
